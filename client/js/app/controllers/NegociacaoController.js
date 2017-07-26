96ca64eed070a50a59654645027310ca58fed0a7
Vinicius Paldes <paldes@izie.com.br>
2017-07-26 20:42:47 -0300

    List added negociacoes into table

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,13 +6,14 @@ class NegociacaoController{
         this._inputValor = $('#valor');
         this._listaNegociacoes = new ListaNegociacoes();
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update();
+        this._negociacoesView.update(this._listaNegociacoes);
     }
         
     adiciona(event) {
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
         this._limpaFormulario();
+        this._negociacoesView.update(this._listaNegociacoes);
         console.log(this._listaNegociacoes);
     }
 
